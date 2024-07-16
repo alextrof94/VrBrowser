cf232afa7ecb2d53e42e313d32da7917f4979555
Александр <alextrof94@gmail.com>
2024-07-16 23:39:15 +0300

    Init

---
+++ VrBrowserTestCore/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace VrBrowserTestCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            BuLoadUrl = new Button();
+            RiCss = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            NuCurvature = new NumericUpDown();
+            NuOffset = new NumericUpDown();
+            label4 = new Label();
+            NuSize = new NumericUpDown();
+            label5 = new Label();
+            TiStartVr = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)NuCurvature).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)NuOffset).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)NuSize).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(49, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(658, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // BuLoadUrl
+            // 
+            BuLoadUrl.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BuLoadUrl.Location = new Point(713, 12);
+            BuLoadUrl.Name = "BuLoadUrl";
+            BuLoadUrl.Size = new Size(75, 23);
+            BuLoadUrl.TabIndex = 1;
+            BuLoadUrl.Text = "Загрузить";
+            BuLoadUrl.UseVisualStyleBackColor = true;
+            BuLoadUrl.Click += BuLoadUrl_Click;
+            // 
+            // RiCss
+            // 
+            RiCss.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            RiCss.Location = new Point(12, 62);
+            RiCss.Name = "RiCss";
+            RiCss.Size = new Size(776, 347);
+            RiCss.TabIndex = 2;
+            RiCss.Text = ".ObsPage__widgetContainer--Ra4Ps {\ndisplay: none;\n}";
+            RiCss.TextChanged += RiCss_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 3;
+            label1.Text = "URL:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 4;
+            label2.Text = "CSS:";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label3.AutoSize = true;
+            label3.Location = new Point(451, 417);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Curvature";
+            // 
+            // NuCurvature
+            // 
+            NuCurvature.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            NuCurvature.DecimalPlaces = 2;
+            NuCurvature.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            NuCurvature.Location = new Point(528, 415);
+            NuCurvature.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
+            NuCurvature.Minimum = new decimal(new int[] { 1, 0, 0, int.MinValue });
+            NuCurvature.Name = "NuCurvature";
+            NuCurvature.Size = new Size(120, 23);
+            NuCurvature.TabIndex = 6;
+            NuCurvature.ValueChanged += NuCurvature_ValueChanged;
+            // 
+            // NuOffset
+            // 
+            NuOffset.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            NuOffset.DecimalPlaces = 2;
+            NuOffset.Increment = new decimal(new int[] { 5, 0, 0, 131072 });
+            NuOffset.Location = new Point(308, 415);
+            NuOffset.Maximum = new decimal(new int[] { 5, 0, 0, 0 });
+            NuOffset.Minimum = new decimal(new int[] { 3, 0, 0, 65536 });
+            NuOffset.Name = "NuOffset";
+            NuOffset.Size = new Size(120, 23);
+            NuOffset.TabIndex = 8;
+            NuOffset.Value = new decimal(new int[] { 45, 0, 0, 131072 });
+            NuOffset.ValueChanged += NuOffset_ValueChanged;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label4.AutoSize = true;
+            label4.Location = new Point(231, 417);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Offset";
+            // 
+            // NuSize
+            // 
+            NuSize.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            NuSize.DecimalPlaces = 2;
+            NuSize.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            NuSize.Location = new Point(89, 415);
+            NuSize.Minimum = new decimal(new int[] { 1, 0, 0, 65536 });
+            NuSize.Name = "NuSize";
+            NuSize.Size = new Size(120, 23);
+            NuSize.TabIndex = 10;
+            NuSize.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            NuSize.ValueChanged += NuSize_ValueChanged;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 417);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Size";
+            // 
+            // TiStartVr
+            // 
+            TiStartVr.Enabled = true;
+            TiStartVr.Interval = 3000;
+            TiStartVr.Tick += TiStartVr_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(794, 450);
+            Controls.Add(NuSize);
+            Controls.Add(label5);
+            Controls.Add(NuOffset);
+            Controls.Add(label4);
+            Controls.Add(NuCurvature);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(RiCss);
+            Controls.Add(BuLoadUrl);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "VrBrowser (by GoodVrGames aka alextrof94)";
+            FormClosing += Form1_FormClosing;
+            Shown += Form1_Shown;
+            ((System.ComponentModel.ISupportInitialize)NuCurvature).EndInit();
+            ((System.ComponentModel.ISupportInitialize)NuOffset).EndInit();
+            ((System.ComponentModel.ISupportInitialize)NuSize).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button BuLoadUrl;
+        private RichTextBox RiCss;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown NuCurvature;
+        private NumericUpDown NuOffset;
+        private Label label4;
+        private NumericUpDown NuSize;
+        private Label label5;
+        private System.Windows.Forms.Timer TiStartVr;
+    }
+}
---
+++ VrBrowserTestCore/Form1.cs
@@ -0,0 +1,392 @@
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using Valve.VR;
+using CefSharp;
+using CefSharp.OffScreen;
+using System.Diagnostics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using VrBrowserTestCore.Properties;
+
+namespace VrBrowserTestCore
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser? browser;
+        CVRSystem? VrSystem = null;
+        uint HmdId;
+
+        bool UrlLoaded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BuLoadUrl_Click(object sender, EventArgs e)
+        {
+            UrlLoaded = false;
+            browser?.LoadUrl(textBox1.Text);
+            Settings.Default.URL = textBox1.Text;
+            Settings.Default.Save();
+        }
+
+        private void RiCss_TextChanged(object sender, EventArgs e)
+        {
+            Settings.Default.CSS = RiCss.Text;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBox1.Text = Settings.Default.URL;
+            RiCss.Text = Settings.Default.CSS;
+            NuCurvature.Value = (decimal)Settings.Default.Curvature;
+            NuSize.Value = (decimal)Settings.Default.Size;
+            NuOffset.Value = (decimal)Settings.Default.Offset;
+            TryToStartMainProcess();
+        }
+
+        private void TiStartVr_Tick(object sender, EventArgs e)
+        {
+            TryToStartMainProcess();
+        }
+
+        private void Browser_FrameLoadEnd(object? sender, FrameLoadEndEventArgs e)
+        {
+            UrlLoaded = true;
+            InjectCSS();
+        }
+
+        private void InjectCSS()
+        {
+            var css = Properties.Settings.Default.CSS;
+            if (!string.IsNullOrEmpty(css))
+            {
+                string script = $@"
+                    (function() {{
+                        var style = document.createElement('style');
+                        style.type = 'text/css';
+                        style.innerHTML = `{css}`;
+                        document.head.appendChild(style);
+                    }})();
+                ";
+                browser.ExecuteScriptAsync(script);
+            }
+        }
+
+        private static bool AssociateOverlayToDevice(ulong overlayHandle, uint deviceIndex)
+        {
+            HmdMatrix34_t transform = new()
+            {
+                m0 = -1,
+                m1 = 0,
+                m2 = 0,
+                m3 = 0,
+                m4 = 0,
+                m5 = -1,
+                m6 = 0,
+                m7 = 0,
+                m8 = 0,
+                m9 = 0,
+                m10 = 1,
+                m11 = -Settings.Default.Offset
+            };
+
+            OpenVR.Overlay.SetOverlayTransformTrackedDeviceRelative(overlayHandle, deviceIndex, ref transform);
+            return true;
+        }
+
+        static int LoadTextureFromBitmap(Bitmap bitmap)
+        {
+            int textureId = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+
+            // Load the image using System.Drawing
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            BitmapData data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            OpenTK.Graphics.OpenGL4.PixelFormat pixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat.Bgra;
+
+            GL.TexImage2D(TextureTarget.Texture2D,
+                0,
+                PixelInternalFormat.Rgba,
+                data.Width,
+                data.Height,
+                0,
+                pixelFormat,
+                PixelType.UnsignedByte,
+                data.Scan0);
+
+            bitmap.UnlockBits(data);
+            bitmap.Dispose();
+
+            // Set texture parameters (optional)
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            return textureId;
+        }
+
+        private void ShowFrameFileFromBitmap(Bitmap bitmap)
+        {
+            int textureId = LoadTextureFromBitmap(bitmap);
+
+            Texture_t texture = new()
+            {
+                eColorSpace = EColorSpace.Auto,
+                eType = ETextureType.OpenGL,
+                handle = (IntPtr)textureId
+            };
+            OpenVR.Overlay.SetOverlayTexture(OverlayHandle, ref texture);
+
+
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+            GL.DeleteTextures(1, ref textureId);
+        }
+
+        private async Task UpdateOverlayWithBrowserContent()
+        {
+            if (!VrStarted) return;
+            if (browser == null || browser.IsDisposed) return;
+            if (!UrlLoaded) return;
+            var bitmap = await browser.ScreenshotAsync();
+            if (bitmap != null)
+            {
+                try
+                {
+                    this.Invoke(
+                        new Action(() =>
+                        {
+                            ShowFrameFileFromBitmap(bitmap);
+                        }));
+                }
+                catch { }
+            }
+            await Task.Delay(16);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (VrStarted)
+            {
+                VrStarted = false;
+                Thread.Sleep(1000);
+                // Cleanup
+                OpenVR.Overlay.HideOverlay(OverlayHandle);
+                OpenVR.Overlay.DestroyOverlay(OverlayHandle);
+            }
+            Cef.Shutdown();
+            OpenVR.Shutdown();
+            if (PreviewGameWindow != null)
+            {
+                PreviewGameWindow.Close();
+                PreviewGameWindow.Dispose();
+                GL.DeleteBuffer(VertexBufferObject);
+                GL.DeleteVertexArray(VertexArrayObject);
+            }
+        }
+
+        // Overlay
+        bool VrStarted = false;
+        ulong OverlayHandle = 0;
+        GameWindow? PreviewGameWindow = null;
+
+        // OpenGL
+        private int VertexArrayObject;
+        private int VertexBufferObject;
+
+
+        static bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
+
+        bool InitVr(out ulong overlayHandle)
+        {
+            overlayHandle = 0;
+            try
+            {
+                if (!IsProcessRunning("vrserver") && !IsProcessRunning("vrcompositor"))
+                    throw new Exception("");
+
+                var systemErr = EVRInitError.None;
+                VrSystem = OpenVR.Init(ref systemErr, EVRApplicationType.VRApplication_Overlay);
+                if (systemErr != EVRInitError.None)
+                    throw new Exception($"OpenVR.Init " + systemErr.ToString());
+
+                uint[] indexes = new uint[1];
+                VrSystem.GetSortedTrackedDeviceIndicesOfClass(ETrackedDeviceClass.HMD, indexes, 0);
+                HmdId = indexes[0];
+
+                // Init VR
+                var overlayErr = EVROverlayError.None;
+                overlayErr = OpenVR.Overlay.CreateOverlay("browser", "browser", ref overlayHandle);
+                if (overlayErr != EVROverlayError.None)
+                {
+                    if (overlayErr == EVROverlayError.KeyInUse)
+                    {
+                        OpenVR.Overlay.FindOverlay("browser", ref overlayHandle);
+                        OpenVR.Overlay.DestroyOverlay(overlayHandle);
+                    }
+                    throw new Exception($"CreateOverlay " + overlayErr.ToString());
+                }
+                OverlayHandle = overlayHandle;
+                overlayErr = OpenVR.Overlay.SetOverlayCurvature(overlayHandle, Settings.Default.Curvature);
+                if (overlayErr != EVROverlayError.None)
+                    throw new Exception($"SetOverlayCurvature " + overlayErr.ToString());
+
+                overlayErr = OpenVR.Overlay.SetOverlayWidthInMeters(overlayHandle, Settings.Default.Size);
+                if (overlayErr != EVROverlayError.None)
+                    throw new Exception($"SetOverlayWidthInMeters " + overlayErr.ToString());
+
+                OpenVR.Overlay.SetOverlayColor(overlayHandle, 1, 1, 1);
+                OpenVR.Overlay.SetOverlayAlpha(overlayHandle, 1);
+
+                OpenVR.Overlay.ShowOverlay(overlayHandle);
+                VRTextureBounds_t bounds;
+                bounds.uMin = 0;
+                bounds.uMax = 1;
+                bounds.vMin = 0;
+                bounds.vMax = 1;
+                OpenVR.Overlay.SetOverlayTextureBounds(overlayHandle, ref bounds);
+
+                AssociateOverlayToDevice(overlayHandle, HmdId);
+                VrStarted = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("VR is not initialized. " + ex.Message);
+                return false;
+            }
+        }
+
+        void InitGL()
+        {
+            var location = new OpenTK.Mathematics.Vector2i(0, -10000);
+
+            NativeWindowSettings nwSettings = new()
+            {
+                Size = new OpenTK.Mathematics.Vector2i(1, 1),
+                Title = "VR Browser Preview",
+                StartVisible = false,
+                WindowBorder = OpenTK.Windowing.Common.WindowBorder.Hidden,
+                Location = location,
+                WindowState = OpenTK.Windowing.Common.WindowState.Minimized,
+                Flags = ContextFlags.Offscreen
+            };
+            PreviewGameWindow = new GameWindow(GameWindowSettings.Default, nwSettings) { };
+
+            GL.ClearColor(new Color4(0.1f, 0, 0, 0.5f));
+
+            // Create vertex array and vertex buffer objects
+            GL.GenVertexArrays(1, out VertexArrayObject);
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.GenBuffers(1, out VertexBufferObject);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            // Define the vertices of a quad
+            float[] vertices = {
+                -1.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f,  1.0f, 0.0f, 0.0f, 0.0f,
+                -1.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f,  1.0f, 0.0f, 0.0f, 0.0f,
+                -1.0f,  1.0f, 0.0f, 1.0f, 0.0f
+            };
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindVertexArray(VertexArrayObject);
+        }
+
+
+        private void TryToStartMainProcess()
+        {
+            if (!InitVr(out OverlayHandle))
+                return;
+            InitGL();
+            TiStartVr.Stop();
+
+            var settings = new CefSettings()
+            {
+                CefCommandLineArgs = { ["enable-media-stream"] = "1", ["autoplay-policy"] = "no-user-gesture-required" }
+            };
+            Cef.Initialize(settings);
+
+            if (string.IsNullOrEmpty(Settings.Default.URL))
+            {
+                browser = new ChromiumWebBrowser()
+                {
+                    Size = new Size(1920, 1080)
+                };
+            }
+            else
+            {
+                browser = new ChromiumWebBrowser(Settings.Default.URL)
+                {
+                    Size = new Size(1920, 1080)
+                };
+            }
+
+            browser.FrameLoadEnd += Browser_FrameLoadEnd;
+
+            Task t = new(() =>
+            {
+                while (true)
+                {
+                    UpdateOverlayWithBrowserContent();
+                }
+            });
+            t.Start();
+        }
+
+        private void NuSize_ValueChanged(object sender, EventArgs e)
+        {
+            Settings.Default.Size = (float)NuSize.Value;
+            Settings.Default.Save();
+            try
+            {
+                if (!VrStarted) return;
+                OpenVR.Overlay.SetOverlayWidthInMeters(OverlayHandle, Settings.Default.Size);
+            }
+            catch { }
+        }
+
+        private void NuOffset_ValueChanged(object sender, EventArgs e)
+        {
+            Settings.Default.Offset = (float)NuOffset.Value;
+            Settings.Default.Save();
+            try
+            {
+                if (!VrStarted) return;
+                AssociateOverlayToDevice(OverlayHandle, HmdId);
+            }
+            catch { }
+        }
+
+        private void NuCurvature_ValueChanged(object sender, EventArgs e)
+        {
+            Settings.Default.Curvature = (float)NuCurvature.Value;
+            Settings.Default.Save();
+            try
+            {
+                if (!VrStarted) return;
+                OpenVR.Overlay.SetOverlayCurvature(OverlayHandle, Settings.Default.Curvature);
+            }
+            catch { }
+        }
+    }
+}
